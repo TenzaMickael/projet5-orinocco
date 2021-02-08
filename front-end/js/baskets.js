fdb9ed7bfacb47cd51821843ffacec9ba3934700
TenzaMickael <62792452+TenzaMickael@users.noreply.github.com>
2021-02-08 17:00:13 +0100

    Update baskets.js

--- front-end/js/baskets.js
+++ front-end/js/baskets.js
@@ -1,5 +1,30 @@
+
+//Import de l'ID "basket" dans le code html 
+
 const basket = document.getElementById("basket");
 console.log(basket)
 
-let teddies = JSON.parse(localStorage.getItem("selectTeddies"));
-console.log(teddies)
+//Requete XMLHttpRequest pour récupérer l'API des oursons
+
+var request = new XMLHttpRequest();                                         
+
+request.onreadystatechange = function () {                                  
+
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {     
+
+        const response = JSON.parse (this.responseText)                 
+        basketTeddies (response); 
+        console.log(response)                                        
+    };
+};
+request.open ("GET" , "http://localhost:3000/api/teddies/");             
+request.send();   
+
+
+
+//Import du localStorage
+
+let selectTeddies = JSON.parse(localStorage.getItem("selectTeddies"));
+console.log(localStorage)
+
+function basketTeddies (data){};
